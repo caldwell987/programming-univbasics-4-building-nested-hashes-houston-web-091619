d94ce7dff0a474f188cd23dd79683e19fe575f9d
David Caldwell <david@avvitt.com>
2019-09-15 03:03:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,8 +33,9 @@ def monopoly_with_third_tier
         :four_pieces_owned=>200
       },
       :names=> {
-      
-        }
+        :reading_railroad=> {
+        "mortgage_value"=>"$100"
+      },
     }
   }
     
